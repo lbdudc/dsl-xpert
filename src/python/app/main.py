27f9c2c1f48fdde93344a96314c75319ae478421
vlamas <vlamas@udc.es>
2024-11-28 16:27:53 +0200

    loading model when the request is done

--- src/python/app/main.py
+++ src/python/app/main.py
@@ -4,9 +4,9 @@ from transformers import pipeline
 app = FastAPI()
 
 # Load model pipeline
-model_pipeline = pipeline("text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
-
-@app.post("/predict/")
+@app.post("/api/chat")
 async def predict(text: str):
+    # We load the pipeline here
+    model_pipeline = pipeline("text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
     result = model_pipeline(text)
     return {"result": result}
